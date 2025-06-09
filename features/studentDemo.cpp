08458c2ace8d91c700a1758cb17f6f87cf0e21f8
KAY KEO <keokay888@gmail.com>
2025-06-09 13:40:15 +0700

    adding another student demo file

---
+++ features/studentDemo.cpp
@@ -0,0 +1,8 @@
+#include<iostream> 
+using namespace std; 
+int main(){
+    cout<<"Hello this is the student demo file."<<endl;
+    cout<<"This file is used to demonstrate the student feature."<<endl;
+    cout<<endl; 
+    return 0; 
+}
