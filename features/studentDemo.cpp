6e8edf96d74c02548d83394155208a9de842679c
KAY KEO <keokay888@gmail.com>
2025-06-09 13:51:48 +0700

    FIX: adding the student class requested

--- features/studentDemo.cpp
+++ features/studentDemo.cpp
@@ -1,5 +1,12 @@
 #include<iostream> 
 using namespace std; 
+class Student {
+    private: 
+        string name; 
+        int age ; 
+    public: 
+        Student(string n , int a) : name(n), age(a) {}
+}; 
 int main(){
     cout<<"Hello this is the student demo file."<<endl;
     cout<<"This file is used to demonstrate the student feature."<<endl;
